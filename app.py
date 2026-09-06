54ac0141956604882908e7dd053c6356ee318399
Gayatri Mehare <gayatrimehare25@gmail.com>
2026-09-06 18:27:17 +0530

    Update model name to gemini-1.5-flash-latest

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if not api_key:
 else:
     # Initialize Gemini API
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel("gemini-1.5-flash")
+    model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
     # Dropdown for Academic Tasks
     task_type = st.selectbox(
